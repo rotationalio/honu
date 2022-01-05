fa9fea293e5ceda893266d178b1cc74192f2c7ba
Benjamin Bengfort <benjamin@bengfort.com>
2022-01-05 11:45:55 -0600

    streaming gossip refactor

--- replica/replica_grpc.pb.go
+++ replica/replica_grpc.pb.go
@@ -11,20 +11,22 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // ReplicationClient is the client API for Replication service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ReplicationClient interface {
-	// During gossip, the initiating replica sends a randomly selected remote peer the
-	// version vectors of all objects it currently stores. The remote peer should
-	// respond with updates that correspond to more recent versions of the objects. The
-	// remote peer can than also make a reciprocal request for updates by sending the
-	// set of versions requested that were more recent on the initiating replica, and
-	// use a partial flag to indicate that it is requesting specific versions. This
-	// mechanism implements bilateral anti-entropy: a push and pull gossip.
-	Gossip(ctx context.Context, in *VersionVectors, opts ...grpc.CallOption) (*Updates, error)
+	// Gossip implements biltateral anti-entropy: during a Gossip session the initiating
+	// replica pushes updates to the remote peer and pulls requested changes. Using
+	// bidirectional streaming, the initiating peer sends data-less sync messages with
+	// the versions of objects it stores locally. The remote replica then responds with
+	// data if its local version is later or sends a sync message back requesting the
+	// data from the initating replica if its local version is earlier (no exchange)
+	// occurs if both replicas have the same version. At the end of a gossip session,
+	// both replicas should have synchronized and have identical underlying data stores.
+	Gossip(ctx context.Context, opts ...grpc.CallOption) (Replication_GossipClient, error)
 }
 
 type replicationClient struct {
@@ -35,27 +37,50 @@ func NewReplicationClient(cc grpc.ClientConnInterface) ReplicationClient {
 	return &replicationClient{cc}
 }
 
-func (c *replicationClient) Gossip(ctx context.Context, in *VersionVectors, opts ...grpc.CallOption) (*Updates, error) {
-	out := new(Updates)
-	err := c.cc.Invoke(ctx, "/honu.replica.v1.Replication/Gossip", in, out, opts...)
+func (c *replicationClient) Gossip(ctx context.Context, opts ...grpc.CallOption) (Replication_GossipClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Replication_ServiceDesc.Streams[0], "/honu.replica.v1.Replication/Gossip", opts...)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	x := &replicationGossipClient{stream}
+	return x, nil
+}
+
+type Replication_GossipClient interface {
+	Send(*Sync) error
+	Recv() (*Sync, error)
+	grpc.ClientStream
+}
+
+type replicationGossipClient struct {
+	grpc.ClientStream
+}
+
+func (x *replicationGossipClient) Send(m *Sync) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *replicationGossipClient) Recv() (*Sync, error) {
+	m := new(Sync)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 // ReplicationServer is the server API for Replication service.
 // All implementations must embed UnimplementedReplicationServer
 // for forward compatibility
 type ReplicationServer interface {
-	// During gossip, the initiating replica sends a randomly selected remote peer the
-	// version vectors of all objects it currently stores. The remote peer should
-	// respond with updates that correspond to more recent versions of the objects. The
-	// remote peer can than also make a reciprocal request for updates by sending the
-	// set of versions requested that were more recent on the initiating replica, and
-	// use a partial flag to indicate that it is requesting specific versions. This
-	// mechanism implements bilateral anti-entropy: a push and pull gossip.
-	Gossip(context.Context, *VersionVectors) (*Updates, error)
+	// Gossip implements biltateral anti-entropy: during a Gossip session the initiating
+	// replica pushes updates to the remote peer and pulls requested changes. Using
+	// bidirectional streaming, the initiating peer sends data-less sync messages with
+	// the versions of objects it stores locally. The remote replica then responds with
+	// data if its local version is later or sends a sync message back requesting the
+	// data from the initating replica if its local version is earlier (no exchange)
+	// occurs if both replicas have the same version. At the end of a gossip session,
+	// both replicas should have synchronized and have identical underlying data stores.
+	Gossip(Replication_GossipServer) error
 	mustEmbedUnimplementedReplicationServer()
 }
 
@@ -63,8 +88,8 @@ type ReplicationServer interface {
 type UnimplementedReplicationServer struct {
 }
 
-func (UnimplementedReplicationServer) Gossip(context.Context, *VersionVectors) (*Updates, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Gossip not implemented")
+func (UnimplementedReplicationServer) Gossip(Replication_GossipServer) error {
+	return status.Errorf(codes.Unimplemented, "method Gossip not implemented")
 }
 func (UnimplementedReplicationServer) mustEmbedUnimplementedReplicationServer() {}
 
@@ -76,36 +101,49 @@ type UnsafeReplicationServer interface {
 }
 
 func RegisterReplicationServer(s grpc.ServiceRegistrar, srv ReplicationServer) {
-	s.RegisterService(&_Replication_serviceDesc, srv)
+	s.RegisterService(&Replication_ServiceDesc, srv)
 }
 
-func _Replication_Gossip_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(VersionVectors)
-	if err := dec(in); err != nil {
+func _Replication_Gossip_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(ReplicationServer).Gossip(&replicationGossipServer{stream})
+}
+
+type Replication_GossipServer interface {
+	Send(*Sync) error
+	Recv() (*Sync, error)
+	grpc.ServerStream
+}
+
+type replicationGossipServer struct {
+	grpc.ServerStream
+}
+
+func (x *replicationGossipServer) Send(m *Sync) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *replicationGossipServer) Recv() (*Sync, error) {
+	m := new(Sync)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
 		return nil, err
 	}
-	if interceptor == nil {
-		return srv.(ReplicationServer).Gossip(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/honu.replica.v1.Replication/Gossip",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ReplicationServer).Gossip(ctx, req.(*VersionVectors))
-	}
-	return interceptor(ctx, in, info, handler)
+	return m, nil
 }
 
-var _Replication_serviceDesc = grpc.ServiceDesc{
+// Replication_ServiceDesc is the grpc.ServiceDesc for Replication service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Replication_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "honu.replica.v1.Replication",
 	HandlerType: (*ReplicationServer)(nil),
-	Methods: []grpc.MethodDesc{
+	Methods:     []grpc.MethodDesc{},
+	Streams: []grpc.StreamDesc{
 		{
-			MethodName: "Gossip",
-			Handler:    _Replication_Gossip_Handler,
+			StreamName:    "Gossip",
+			Handler:       _Replication_Gossip_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
 	Metadata: "replica/v1/replica.proto",
 }
