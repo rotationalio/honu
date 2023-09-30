f88b76b610b707adef55cc74f66c5bd35f15c5af
Benjamin Bengfort <benjamin@bengfort.com>
2023-09-30 13:08:06 -0700

    Service Protocol Buffers

--- proto/generate.go
+++ proto/generate.go
@@ -1,3 +1,3 @@
 package pb
 
-//go:generate protoc -I . --go_out=.. --go_opt=module=github.com/rotationalio/honu --go-grpc_out=.. --go-grpc_opt=module=github.com/rotationalio/honu object/v1/object.proto replica/v1/replica.proto
+//go:generate protoc -I . --go_out=.. --go_opt=module=github.com/rotationalio/honu --go-grpc_out=.. --go-grpc_opt=module=github.com/rotationalio/honu object/v1/object.proto replica/v1/replica.proto pagination/v1/pagination.proto peers/v1/peers.proto honu/v1/honu.proto
