598f082503a15afe6c602a60c964ba84bfd8a1c8
Benjamin Bengfort <benjamin@bengfort.com>
2024-10-19 06:52:34 -0500

    Fix protobuf links

--- replica/generate.sh
+++ replica/generate.sh
@@ -13,12 +13,15 @@ fi
 
 MODULE="github.com/rotationalio/honu/replica/v1"
 APIMOD="github.com/rotationalio/honu/replica/v1;replica"
+OBJMOD="github.com/rotationalio/honu/object/v1;object"
 
 # Generate the protocol buffers
 protoc -I=${PROTOS} \
     --go_out=./v1 --go-grpc_out=./v1 \
     --go_opt=module=${MODULE} \
     --go-grpc_opt=module=${MODULE} \
+    --go_opt=Mobject/v1/object.proto="${OBJMOD}" \
     --go_opt=Mreplica/v1/replica.proto="${APIMOD}" \
+    --go-grpc_opt=Mobject/v1/object.proto="${OBJMOD}" \
     --go-grpc_opt=Mreplica/v1/replica.proto="${APIMOD}" \
     replica/v1/replica.proto
